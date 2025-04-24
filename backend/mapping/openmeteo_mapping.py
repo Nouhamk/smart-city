47707815465090fe834a787d4863bc6df2f831c3
XXGIGASHADOWPAUL38XX <tristan.ginet@etu.univ-smb.fr>
2025-04-24 16:22:25 +0200

    newer version (not runnable)

---
+++ backend/mapping/openmeteo_mapping.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+openmeteo_mapping_attributes = {
+    "temperature_2m": "temperature",
+    "apparent_temperature": "apparent_temperature",
+    "relative_humidity_2m": "humidity",
+    "dewpoint_2m": "dewpoint",
+    "precipitation_probability": "precipitation_probability",
+    "precipitation": "precipitation",
+    "snowfall": "snow",
+    "snow_depth": "snow depth",
+    "wind_gust_10m": "wind_gust",
+    "wind_speed_10m": "wind_speed",
+    "wind_direction_10m": "wind_direction",
+    "surface_pressure": "pressure",
+    "visibility": "visibility",
+    "cloud_cover": "cloudcover"
+}
+
+
+def map_to_common_data(openmeteo_data):
+    common_data = {}
+    common_data["latitude"] = openmeteo_data["latitude"]
+    common_data["longitude"] = openmeteo_data["longitude"]
+    common_data["unit"] = openmeteo_data["hourly_units"]["temperature_2m"]
+    common_data["data"] = [
+        {
+            "time": openmeteo_data["hourly"]["time"][index],
+            "data": {
+                openmeteo_mapping_attributes[key]:
+                    value[index] for key, value in filter(lambda x: x[0] != "time", openmeteo_data["hourly"].items())
+            }
+        }
+        for index in range(len(openmeteo_data["hourly"]["time"]))
+    ]
+
+    return common_data
+
+
+def convert_to_df(openmeteo_data):
+    df = pd.DataFrame(openmeteo_data["hourly"])
+    df['time'] = pd.to_datetime(df['time'])
+    return df
---
+++ backend/openmeteo.py
@@ -0,0 +1,19 @@
+import requests
+
+from backend.mapping.openmeteo_mapping import map_to_common_data, convert_to_df
+
+latitude = 40.7128  # Example: Latitude for New York City
+longitude = -74.0060  # Example: Longitude for New York City
+start_date = "2025-02-20"
+end_date = "2025-03-01"
+metrics = "temperature_2m,relative_humidity_2m"
+
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relative_humidity_2m"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    df_data = convert_to_df(data)
+    df_data.to_csv('./csv/openmeteo.csv', index=False)
+else:
+    print("Error:", response.status_code, response.text)
+
