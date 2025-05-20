7e0084f8d0875f85c4d9da86a036598c397b19c5
hugo-rgr <hugo@silk-digital.com>
2025-05-20 15:07:21 +0200

    Setup backend supabase

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from .default_view import default_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', default_view, name='home'),
 ]
